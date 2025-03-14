925abf70ac77f74b1dfce71a76f365931e9291f4
pchanbro <cksgud0509@naver.com>
2025-03-15 01:01:28 +0900

    ADD : 프로젝트 생성 및 캐릭터 구현

---
+++ Sparta_Intern_2DTopDown/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Vector2 moveDir;
+    public float speed;
+    SpriteRenderer playerSprite;
+
+    void Awake()
+    {
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if(moveDir.x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        else if(moveDir.x > 0)
+        {
+            playerSprite.flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += (new Vector3(moveDir.x, moveDir.y)).normalized * Time.fixedDeltaTime * speed;
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveDir = value.Get<Vector2>();
+    }
+}
